abdb9c508a9b5e409ae874e7681975ae78d1b1b1
Luis Nunez <a.nunez.jeria@gmail.com>
2022-12-14 15:40:37 -0300

    app movies problemas resueltos

---
+++ 07-PelisApp/src/api/movieDB.tsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+const movieDB = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/movie',
+    params: {
+        api_key: '6ff275cf73e920f37a91bec08a19bc40',
+        language: 'es-ES'
+    }
+});
+
+
+export default movieDB;
